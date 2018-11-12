566a0b275c1948c6cb5c9d38bd022fc8871b822a
Viskari Tuomas <tuomas.viskari@digia.com>
2018-11-12 11:56:42 +0200

    Initial commit

---
+++ src/DesktopLabel/Label.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DesktopLabel
+{
+    partial class Label
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Label));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnChangeName = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 15);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(187, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // btnChangeName
+            // 
+            this.btnChangeName.Location = new System.Drawing.Point(219, 15);
+            this.btnChangeName.Name = "btnChangeName";
+            this.btnChangeName.Size = new System.Drawing.Size(43, 23);
+            this.btnChangeName.TabIndex = 1;
+            this.btnChangeName.Text = "...";
+            this.btnChangeName.UseVisualStyleBackColor = true;
+            this.btnChangeName.Click += new System.EventHandler(this.btnChangeName_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(12, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(79, 29);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "label1";
+            // 
+            // Label
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(274, 46);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnChangeName);
+            this.Controls.Add(this.txtName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Label";
+            this.Text = " ";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Label_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnChangeName;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ src/DesktopLabel/Label.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopLabel
+{
+    public partial class Label : Form
+    {
+        public Label()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_Load(object sender, EventArgs e)
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width,
+                                      workingArea.Bottom - Size.Height);
+
+            lblName.Height = this.Size.Height;
+            lblName.Width = this.Size.Height;
+            lblName.TextAlign = ContentAlignment.MiddleCenter;
+
+            lblName.Hide();
+
+            txtName.KeyPress += txtLocal_KeyPress;
+            lblName.Click += lblName_Click;
+            //lblName.MouseHover += HideForm;
+        }
+
+        private void HideForm(object sender, EventArgs e)
+        {
+            this.Hide();
+            Thread.Sleep(3000);
+            this.Show();
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+            lblName.Hide();
+            txtName.Show();
+            btnChangeName.Show();
+            txtName.Text = "";
+        }
+
+        private string _desktopName = "";
+
+        private void txtLocal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)//Enter{
+            {
+                e.Handled = true;
+                ChangeName();
+            }
+        }
+    
+
+        private void btnChangeName_Click(object sender, EventArgs e)
+        {
+            ChangeName();
+        }
+
+        private void ChangeName()
+        {
+            _desktopName = txtName.Text;
+            lblName.Text = _desktopName;
+
+            txtName.Hide();
+            btnChangeName.Hide();
+            lblName.Show();
+        }
+
+        private void DesktopLabel_Click(object sender, EventArgs e)
+        {
+            txtName.Show();
+            btnChangeName.Show();
+            lblName.Hide();
+        }
+    }
+}
